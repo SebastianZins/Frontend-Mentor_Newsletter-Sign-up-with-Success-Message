c373b5d499c3c9130bdc0ce6c0412e54c2d38b14
SebastianZins <s-zins@web.de>
2024-01-23 03:17:56 +0100

    added success message and transition in mobile view

---
+++ scripts/script.js
@@ -0,0 +1,40 @@
+/*
+ * triggered by subscribe and dismiss button
+ */
+function toggle_successMessageActive() {
+    // check if mail input is valid
+    const isValid = check_validInput();
+    if (!isValid) {
+        // if invalid show error and skip
+        document.getElementById('notice-label').classList.add('invalid');
+        return;
+    }
+
+    // check if success message is active / is being shown
+    const isActive = document
+        .getElementById('success-message')
+        .classList.contains('active');
+
+    if (isActive) {
+        // if active dismiss success message
+        document.getElementById('success-message').classList.remove('active');
+    } else {
+        // if not active show success message and remove invalid label
+        document.getElementById('success-message').classList.add('active');
+        document.getElementById('notice-label').classList.remove('invalid');
+    }
+}
+
+/*
+ * check validity of mail input string
+ * by validating agaings mail regex and test if empty
+ */
+function check_validInput() {
+    const value = document.getElementById('mail-input').value;
+    const isNonEmpty = value.length > 0;
+
+    const re = /\S+@\S+\.\S+/;
+    const isEmail = re.test(value);
+
+    return isNonEmpty && isEmail;
+}
